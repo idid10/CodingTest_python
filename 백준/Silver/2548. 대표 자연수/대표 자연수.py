eae4af0a695ccee1a9dd2bf1f7cba764c3be449c
Young Shin <111336233+idid10@users.noreply.github.com>
2025-01-07 20:40:05 +0900

    [Silver III] Title: 대표 자연수, Time: 36 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.py
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.py
@@ -1,10 +1,17 @@
+# 입력 받기
 import sys
+input = sys.stdin.read
 
-input = sys.stdin.readline
-n = int(input())
-num = list(map(int, input().split()))
-num.sort()
-if n%2 == 0:
-    print(num[n//2 -1])
-else:
-    print(num[n//2])
+data = input().split()
+N = int(data[0])  # 숫자의 개수
+numbers = list(map(int, data[1:]))
+
+# 수열 정렬
+numbers.sort()
+
+# 중앙값 찾기
+# N이 짝수일 경우, 두 중앙값 중 작은 값 사용 (정렬 기준으로 자동으로 작은 값 선택)
+median = numbers[(N - 1) // 2]
+
+# 결과 출력
+print(median)
