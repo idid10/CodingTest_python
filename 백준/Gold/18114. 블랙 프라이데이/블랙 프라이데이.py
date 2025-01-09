c5fa7b4840616d87cb8aee7ec34fee7dbd1708d6
Young Shin <111336233+idid10@users.noreply.github.com>
2025-01-09 19:38:13 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
@@ -1,37 +1,37 @@
 import sys
 input = sys.stdin.readline
-n,c = map(int,input().split())
-data = list(map(int,input().split()))
-data.sort()
+N, C = map(int,input().split())
+weights = list(map(int,input().split()))
+weights.sort()
 
 def binary_search(left,right,diff):
     while left <= right:
-        mid = (left + right) // 2
-        if data[mid] == diff:
+        mid = (left + right)//2
+        if weights[mid] == diff:
             return 1
-        elif data[mid] > diff:
+        elif weights[mid] > diff:
             right = mid - 1
         else:
             left = mid + 1
     return 0
 
-def check(n,c):
-    if c in data:
+def check(N, C):
+    if C in weights:
         return 1
-    i, j = 0, n-1
+    i, j = 0, N-1
     while i < j:
-        total = data[i] + data[j]
-        if total > c:
+        total = weights[i] + weights[j]
+        if total > C:
             j -= 1
-        elif total == c:
+        elif total == C:
             return True
         else:
-            diff = c - total
-            if data[i] != diff and data[j] != diff and binary_search(i,j,diff):
+            diff = C - total
+            if weights[i] != diff and weights[j] != diff and binary_search(i,j,diff):
                 return True
             i += 1
           
-if check(n,c):
+if check(N, C):
     print(1)
 else:
     print(0)
